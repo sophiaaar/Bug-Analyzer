b35a605e186b3bde1ca71c816d68c87af43ee3b6
Sophia Clarke <sophiaclarke93@gmail.com>
2018-05-31 13:28:47 +0100

    Started work on getting information from TestRail
    Leaving in my comments for now in case I need to use the code later

--- FogBugzCases.py
+++ FogBugzCases.py
@@ -1,9 +1,38 @@
 from fogbugz import FogBugz
+from testrail import *
 import csv
 import sys
 
-# Fill in the following values as appropriate to your installation
-S_FOGBUGZ_URL   = 'https://fogbugz.unity3d.com'
+client = APIClient('')
+client.user = ''
+client.password = ''
+
+plans = client.send_get('get_plans/2')
+#tests = client.send_get('get_tests/5696')
+#print(plans)
+#plansStr = ''.join((str(v) for v in plans))
+
+#planIDsList = list()
+
+for plan in plans:
+	planID = plan["id"]
+	planWithEntries = client.send_get('get_plan/'+planID)
+	#print(planID)
+#	planIDsList.append(planID)
+
+#for plan_id in planIDsList:
+#	planWithEntries = client.send_get('get_plan/'+plan_id)
+
+#file = open("plansOutput.txt", "w")
+#file.write(plansStr)
+#file.close()
+
+#file2 = open("testsOutput.txt", "w")
+#testsStr = ''.join((str(x) for x in tests))
+#file2.write(testsStr)
+#file2.close()
+
+S_FOGBUGZ_URL   = ''
 TOKEN			= ""
 S_EMAIL         = ''
 S_PASSWORD      = ''
